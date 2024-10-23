3bc94c3d82d51ecc6b1ae408c600871505b9f21e
CitraAndini <citraandini1849@gmail.com>
2024-10-23 14:39:22 +0700

    Add files via upload

---
+++ Latihan 26 coding _Gaji Pegawai PT Telkom_ menggunakan array .cpp
@@ -0,0 +1,124 @@
+ 
+//di buat oleh Citra Andini 
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+int main(){
+	cout << setprecision(13);
+	const int btspg = 100;
+	string nama[btspg],gol[btspg],kode[btspg],jabatan[btspg];
+	float gaji[btspg],tunjangan[btspg],pinjaman[btspg],pajak[btspg],gkotor[btspg],gbersih[btspg],total;
+	char lagi;
+	int jumpeg = 0;
+	cout << "             GAJI PEGAWAI PT. TELKOM" << endl;
+	cout << "---------------------------------------------------" << endl;
+	
+	do{
+		
+
+		
+		cout << "Pegawai ke-" << jumpeg + 1 << endl;
+		cout << "Nama Pegawai             : ";
+		cin >> nama[jumpeg];
+		cout << "Golongan                 : ";
+		cin >> gol[jumpeg];
+		cout << "Kode Jabatan             : ";
+		cin >> kode[jumpeg];
+		
+		if(gol[jumpeg] == "Ia"){
+			gaji[jumpeg] = 500000;
+		}
+		else if(gol[jumpeg] == "IIa"){
+			gaji[jumpeg] = 750000;
+		}
+		else if(gol[jumpeg] == "IIIa"){
+			gaji[jumpeg] = 1000000;
+		}
+		else if(gol[jumpeg] == "Ib"){
+			gaji[jumpeg] = 150000;
+		}
+		else if(gol[jumpeg] == "IIb"){
+			gaji[jumpeg] = 1750000;
+		}
+			else if(gol[jumpeg] == "IIIb"){
+			gaji[jumpeg] = 2500000;
+		}
+		else{
+			gaji[jumpeg] = 0;
+		}
+		
+		if(kode[jumpeg] == "Dir"){
+			jabatan[jumpeg] = "Direktur";
+			tunjangan[jumpeg] = 450000;
+		}
+		else if(kode[jumpeg] == "Sek"){
+			jabatan[jumpeg] = "Sekretaris";
+			tunjangan[jumpeg] = 300000;
+		}
+		else if(kode[jumpeg] == "Wak"){
+			jabatan[jumpeg] = "Wakil Direktur";
+			tunjangan[jumpeg] = 375000;
+		}
+		else if(kode[jumpeg] == "Stf"){
+			jabatan[jumpeg] = "Staf Administrasi";
+			tunjangan[jumpeg] = 200000;
+		}
+		else{
+			jabatan[jumpeg] = "-";
+			tunjangan[jumpeg] = 0;
+		}
+		
+		pajak[jumpeg] = gaji[jumpeg] * 0.05;
+		gkotor[jumpeg] = gaji[jumpeg] + tunjangan[jumpeg];
+	
+		
+		
+		cout << "Jabatan                  : " << jabatan[jumpeg] << endl;
+		cout << "Gaji                     : " << gaji[jumpeg]<< endl;
+		cout << "Tunjangan                : " << tunjangan[jumpeg]<< endl;
+		cout << "Pinjaman                 : ";
+		cin >> pinjaman[jumpeg];
+		
+		cout << "Pajak                    : " << pajak[jumpeg]<< endl;
+		cout << "Total Gaji Kotor         : " << gkotor[jumpeg]<< endl;
+		gbersih[jumpeg] = gkotor[jumpeg] - pinjaman[jumpeg] - pajak[jumpeg];
+		cout << "Total Gaji Bersih        : " << gbersih[jumpeg]<< endl;
+		cout << endl;
+		cout << "---------------------------------------------------" << endl;
+			total = total + gbersih[jumpeg];
+		jumpeg++;
+		cout << endl;
+		cout << "Masih Ingin Menghitung Gaji Lagi[Y/T]: ";
+		cin>> lagi;
+		cout << endl;
+	}while(lagi == 'Y' || lagi == 'y');
+
+	cout << "                                Laporan PT. TELKOM" << endl;
+	cout << "\n================================================================================\n " << endl;
+	cout 
+		<< left << setw(2) <<""
+		<<setw(9) << "Nama "
+		<< setw(18) << "Jabatan "
+		<< setw(9) << "Gaji "
+		<< setw(14) << "Pinjaman "
+		<< setw(14) << "Gaji Kotor "
+		<< setw(14) << "Gaji Bersih " << endl;
+	cout << "\n================================================================================\n " << endl;
+		
+	for(int i = 0; i < jumpeg; i++){
+		cout << endl;
+		cout << left << setw(2) <<""
+		<< setw(9) << nama[i]
+		<< setw(18) << jabatan[i]
+		<< setw(9) << gaji[i]
+		<< setw(14) << pinjaman[i]
+		<< setw(14) << gkotor[i]
+		<< setw(14) << gbersih[i] << endl;
+
+	}
+	cout << "\n================================================================================\n " << endl;
+	cout <<right<< setw(66) << "Total Gaji Keseluruhan: " << total;
+	return 0;
+}
+    
---
+++ Uts Citra Anidini _coding hasil pembayaran uang kuliah_.cpp
@@ -0,0 +1,110 @@
+
+//dibuat oleh Citra Andini
+
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    cout<< setprecision(10);
+    const int maxmhs =50;
+    string nama[maxmhs],kode[maxmhs],jurusan[maxmhs];
+    float npm[maxmhs],pembwal[maxmhs],jumcilan[maxmhs],bescilan[maxmhs],pembayaran[maxmhs],uangkul[maxmhs],uangkulter[maxmhs],ss_uangkul[maxmhs],sisaungkul[maxmhs];      
+    char lagi;
+    int jumas=0;
+    cout << "PEMBAYARAN UANG KULIAH" << endl;
+    cout << "-----------------------" << endl;
+    
+    do {
+    
+    cout << "Nama: ";
+    cin >> nama[jumas];
+    cout << "NPM: ";
+    cin >> npm[jumas];
+    cout << "kode: ";
+    cin >> kode[jumas];
+    
+     if (kode[jumas] == "SI") {
+            jurusan[jumas] = "Sistem Infomasi";
+            pembwal[jumas] = 2100000;
+            jumcilan[jumas] = 7;
+            bescilan[jumas] =300000;
+        } else if (kode[jumas] == "TI") {
+            jurusan [jumas] = "Teknik Informatika ";
+            pembwal[jumas] = 250000;
+            jumcilan[jumas] = 7;
+            bescilan[jumas] = 300000;
+        } else if (kode[jumas] == "KA") {
+            jurusan [jumas] = "Komputer Akuntansi";
+            pembwal[jumas] = 1750000;
+            jumcilan[jumas] = 6;
+            bescilan[jumas] = 200000;
+        } else if (kode[jumas] == "MI") {
+            jurusan [jumas] = "Manajemen Informatika";
+            pembwal[jumas] = 1500000;
+            jumcilan[jumas] = 6;
+            bescilan[jumas] = 150000;
+        } else {
+            pembwal[jumas] = 0;
+            jumcilan[jumas] = 0;
+            bescilan[jumas] = 0;
+        }    
+        
+       uangkul[jumas] = pembwal[jumas] + (jumcilan[jumas]*bescilan[jumas]);
+       uangkulter [jumas] = pembwal[jumas] + (pembayaran [jumas]*bescilan[jumas]);
+       sisaungkul[jumas] = uangkul[jumas] - uangkulter[jumas];
+        
+        cout << "jurusan                       : " << jurusan [jumas] << endl;
+        cout << "pembayaran Awal RP            : " << pembwal [jumas] << endl;
+        cout << "Jumlah cicilan                : " << jumcilan[jumas] << endl;
+        cout << "Besar cicilan                 : " << bescilan[jumas] << endl;
+        cout << "pembayaran ke- "<< endl;
+        cin >> pembayaran [jumas];
+        cout << "Uang kuliah                   : " << uangkul[jumas] << endl;
+        cout << "Uang kuliah terbayar          : " << uangkulter[jumas] << endl;
+        cout << "Sisa uang kuliah.             : " << sisaungkul[jumas] << endl;
+        
+        cout << endl;
+        
+       jumas++;
+       cout << "Masih ingin lanjut menghitung? [Y/T]: ";
+       cin >> lagi;
+       cout << endl;
+
+      } while (lagi == 'Y' || lagi == 'y');
+      
+     cout<< "\n==================================================================\n";
+     cout<<"                   LAPORAN HASIL PEMBAYARAN UANG KULIAH ";
+     cout<< "\n==================================================================\n";
+     cout<< setw(5) << left<<"nama "
+         << setw(5) << "jurusan "
+         << setw(5) << "Pembayaran Awal RP "
+         << setw(5) << "Jumlah cicilan "
+         << setw(5) << "Besar cicilan "
+         << setw(5) << "Uang kuliah "
+         << setw(5) << "Uang kuliah terbayar "
+         << setw(5) << "Sisa uang kuliah "<< endl;
+     cout<< "\n==================================================================\n";
+     
+     for(int i = 0; i < jumas; i++){
+     
+     cout<< setw(5) << left<<nama[i]<<" "
+         << setw(5) << jurusan [i]<<" "
+         << setw(5) << pembwal[i]<<" "
+         << setw(5) << pembayaran[i]<<" "
+         << setw(5) << jumcilan[i]<<" "
+         << setw(5) << bescilan[i]<<" "
+         << setw(5) << uangkul[i]<<" "
+         << setw(5) << uangkulter[i]<<" "
+         << setw(5) << sisaungkul[i]<<" "<<endl;
+    }
+     cout<< "\n==================================================================\n";
+     
+     
+ return 0;
+}
+
+
+    
