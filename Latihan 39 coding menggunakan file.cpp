c34a93ad334f81aadcfff09ac5d3ad23d70e2386
CitraAndini <citraandini1849@gmail.com>
2024-11-29 09:02:48 +0700

    Add files via upload

---
+++ Latihan 39 coding menggunakan file.cpp
@@ -0,0 +1,25 @@
+//dibuat oleh Citra Andini 
+//coding menggunakan file
+// 1 dimensi
+
+#include <iostream>
+#include <fstream> //untuk baca data
+
+using namespace std;
+
+int main(){
+	fstream file("data.txt");
+	string line;
+	
+	if(file.is_open()){
+		while(getline(file,line)){
+		cout << line << endl;
+	}
+	file.close();
+}else{
+	cout << "file tidak bisa dibuka";
+}
+return 0;
+
+}
+		
---
+++ latihan 40 coding menggunakan file.cpp
@@ -0,0 +1,26 @@
+//dibuat oleh Citra Andini 
+//coding menggunakan file
+
+#include <iostream>
+#include <fstream> //untuk baca data
+
+using namespace std;
+
+int main(){
+	ofstream file("data.txt");
+	
+	if(file.is_open()){
+		file << "STMIK Penusa\n";
+		file << "Belajar Algoritma Pemrograman.";
+	file.close();
+}else{
+	cout << "file tidak bisa dibuka";
+}
+return 0;
+
+}
+		
+
+
+
+		
